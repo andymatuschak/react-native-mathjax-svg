2ec6da205bbe4af876b2723b43466d50732a059e
Andy Matuschak <andy@andymatuschak.org>
2023-05-17 10:40:29 -0700

    Added display to TS types

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,7 @@ export type MathJaxProps = Omit<XmlProps, "xml"> & {
   color?: string;
   fontSize?: number;
   children?: string;
+  display?: boolean;
 };
 
 export default function MathJax(props: MathJaxProps): ReactElement;
